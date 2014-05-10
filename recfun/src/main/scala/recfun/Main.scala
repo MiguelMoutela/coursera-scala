1ba92f7e3659190179a77c8277f59ace95d8ce5c
robertorobles <info@robertorobles.es>
2014-05-10 11:45:52 +0200

    Week 1: Functions & Evaluations - Resolved

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,28 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = if (c==0 || c==r) 1 else pascal(c-1,r-1)+pascal(c,r-1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = balance(chars, 0)
+  
+  def balance(chars: List[Char], parentheses: Int): Boolean =
+    if (parentheses == -1) false
+    else if (chars.isEmpty) parentheses == 0
+    else balance(chars.tail, parentheses+getBalanceNumber(chars.head))
+  
+  def getBalanceNumber(head: Char): Int =
+    if (head == '(') 1
+    else if (head == ')') -1
+    else 0
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (money < 0 || coins.isEmpty) 0
+    else if (money == 0) 1
+    else countChange(money-coins.head, coins) + countChange(money, coins.tail)
 }
